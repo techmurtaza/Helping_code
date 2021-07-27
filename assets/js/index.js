f921933cfbdfd83c46afff81bf5a0241ea255240
murtuzz <lightwalamurtaza5210@gmail.com>
2021-07-27 14:00:45 +0530

    remove click to copy and added copy pointer

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,46 +5,46 @@ $(function(){
 	$('.na').click(function(){
         console.log('inside link funtion');
     
-    	if(flag){
-        	
-        	$('#navbtn').click();
-        	flag=false;
-        	console.log('inside link funtion if');
-    	}
+		if(flag){
+			
+			$('#navbtn').click();
+			flag=false;
+			console.log('inside link funtion if');
+		}
 	
 	})
-    	
+
 	$('#navbtn').click(function(){
 		flag=true;
-    	console.log('inside navbtn clicked');
+		console.log('inside navbtn clicked');
 	})
 
 
 	$('pre').click(function(){
 
 		var $temp = $("<input>");
-	    $("body").append($temp);
-	    $temp.val($(this).text()).select();
-	    console.log($temp.val());
-	    document.execCommand("copy");
-	    $temp.remove();
+		$("body").append($temp);
+		$temp.val($(this).text()).select();
+		console.log($temp.val());
+		document.execCommand("copy");
+		$temp.remove();
 	})
 
-	$('pre').hover(function(){
-		if($('body').find('#div').length){
-			console.log("inside")
+	// $('pre').hover(function(){
+	// 	if($('body').find('#div').length){
+	// 		console.log("inside")
 
-		}else{
-			var div = $("<div id=\"div\"><span class=\"text-white\">click to copy</span></div>")
-			div.appendTo(this.parentNode);
-		}
-		console.log(div);
-	},
-	function(){
+	// 	}else{
+	// 		var div = $("<div id=\"div\"><span class=\"text-white\">click to copy</span></div>")
+	// 		div.appendTo(this.parentNode);
+	// 	}
+	// 	console.log(div);
+	// },
+	// function(){
 
-		if($('body').find('#div').length){
-			$('#div').remove();
-		}
-	})
+	// 	if($('body').find('#div').length){
+	// 		$('#div').remove();
+	// 	}
+	// })
 
 })
